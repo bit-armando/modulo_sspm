edae4eebdf2771839b1ac3c0a44208b4961a12c7
Armando Rosales <refill.1509@gmail.com>
2023-12-28 14:58:21 -0600

    creacion de views con jinja y el requirements

--- modulo/home/models.py
+++ modulo/home/models.py
@@ -1,3 +1,15 @@
+# Modelo de los visitantes
 from django.db import models
 
-# Create your models here.
+class Visitantes(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    departamento_destino = models.CharField(max_length=50)
+    motivo_visita = models.CharField(max_length=100)
+    folio_gafete = models.IntegerField()
+    # dni = models.CharField(max_length=8)
+    fecha_entrada = models.DateTimeField(auto_now_add=True)
+    fecha_salida = models.DateTimeField(blank=True, null=True)  
+
+    def __str__(self):
+        return self.nombres + ' ' + self.apellidos
--- modulo/home/views.py
+++ modulo/home/views.py
@@ -1,6 +1,25 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+import cv2
+
+from .models import Visitantes
 
 # Vista de la ingesta de los datos
 def index(request):
-    return HttpResponse("Holi Marco UwU.")
+    template = loader.get_template('home/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def capturar_imagen(request):
+    template = loader.get_template('home/capturar_imagen.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def salida(request):
+    visitantes_activos = Visitantes.objects.filter(fecha_salida__isnull=True)
+    template = loader.get_template('home/salida.html')
+    context = {
+        'visitantes_activos': visitantes_activos,
+    }
+    return HttpResponse(template.render(context, request))
