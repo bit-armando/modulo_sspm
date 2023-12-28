23164a8fdf563f4561d6eaf2a5f84db73dba8f27
Armando Rosales <refill.1509@gmail.com>
2023-12-28 11:51:42 -0600

    jugando UwU

--- modulo/home/views.py
+++ modulo/home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Vista de la ingesta de los datos
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Holi Marco UwU.")
