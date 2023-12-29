2b01b7308d3ff5f5d3badb4e89c0ae76297772c4
Armando Rosales <refill.1509@gmail.com>
2023-12-29 15:58:11 -0600

    Se terminaron los acces point de registrar salida y el de registrar entrada

--- modulo/home/urls.py
+++ modulo/home/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('salida/', views.salida, name='salida')
+    path('salida/', views.salida, name='salida'),
+    path('marcar_salida/<int:visitante_id>/', views.marcar_salida, name='marcar_salida'),
+    path('registro_entrada/', views.registrar_entrada, name='registrar_entrada')
 ]
--- modulo/home/views.py
+++ modulo/home/views.py
@@ -1,25 +1,38 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
 import cv2
 
 from .models import Visitantes
 
 # Vista de la ingesta de los datos
 def index(request):
-    template = loader.get_template('home/index.html')
     context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'home/index.html', context)
 
-def capturar_imagen(request):
-    template = loader.get_template('home/capturar_imagen.html')
+def registrar_entrada(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombres']
+        apellido = request.POST['apellido_paterno'] + ' ' + request.POST['apellido_materno']
+        departamento = request.POST['departamento']
+        motivo = request.POST['motivo']
+        folio = request.POST['folio_gafete']
+        visitante = Visitantes(nombres=nombre, apellidos=apellido, departamento_destino=departamento, motivo_visita=motivo, folio_gafete=folio)
+        visitante.save()
+        
+        
+        return render(request, 'home/index.html', {})
     context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'home/index.html', context)
 
 def salida(request):
     visitantes_activos = Visitantes.objects.filter(fecha_salida__isnull=True)
-    template = loader.get_template('home/salida.html')
     context = {
         'visitantes_activos': visitantes_activos,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'home/salida.html', context)
+def marcar_salida(request, visitante_id):
+    visitante = Visitantes.objects.get(pk=visitante_id)
+    visitante.fecha_salida = timezone.now()
+    visitante.save()
+    return redirect('salida')
